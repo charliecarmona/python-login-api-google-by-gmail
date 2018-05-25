0994f8bbd211b61c04b290da8ee223ae1012d2f7
charlie carmona <36017940+charliecarmona@users.noreply.github.com>
2018-05-25 08:54:56 -0500

    Add files via upload
    Modificación de index  y cambio de re dirección

--- app.py
+++ app.py
@@ -1,22 +1,12 @@
 import os
 
 from flask import Flask, jsonify
-from proveedor import GoogleLogin, LinkedInLogin, FacebookLogin
-
-
+from proveedor import GoogleLogin
 
 
 
 os.environ["GOOGLE_LOGIN_CLIENT_ID"] = "364672633912-hbhtatjd9kbkbeaocn0902pivum4t0cd.apps.googleusercontent.com"
 os.environ["GOOGLE_LOGIN_CLIENT_SECRET"] = "b_HGPbqiY-gBHSCBuS38fVau"
-
-os.environ["LINKEDIN_LOGIN_CLIENT_ID"] = "-"
-os.environ["LINKEDIN_LOGIN_CLIENT_SECRET"] = "-"
-
-os.environ["FACEBOOK_LOGIN_CLIENT_ID"] = "-"
-os.environ["FACEBOOK_LOGIN_CLIENT_SECRET"] = "-"
-
-
 
 
 app = Flask(__name__)
@@ -28,20 +18,13 @@ app.config.update(
 
 for config in (
   "GOOGLE_LOGIN_CLIENT_ID",
-  "GOOGLE_LOGIN_CLIENT_SECRET",
-  "LINKEDIN_LOGIN_CLIENT_ID",
-  "LINKEDIN_LOGIN_CLIENT_SECRET",
-  "FACEBOOK_LOGIN_CLIENT_ID",
-  "FACEBOOK_LOGIN_CLIENT_SECRET"
+  "GOOGLE_LOGIN_CLIENT_SECRET"
 
 ):app.config[config] = os.environ[config]
 
 
 
-
 google_login = GoogleLogin(app)
-linkedin_login = LinkedInLogin(app)
-facebook_login = FacebookLogin(app)
 
 
 
@@ -50,10 +33,8 @@ facebook_login = FacebookLogin(app)
 def index():
   return """
 <html>
-<a href="{}">Login con con cuenta de Google </a><br>
-<a href="{}">Login con Linkedin</a> <br>
-<a href="{}">Login con Facebook</a> <br>
-""".format(google_login.authorization_url(), linkedin_login.authorization_url(), facebook_login.authorization_url())
+<a href="{}">Python Login con con cuenta de Google </a>
+""".format(google_login.authorization_url())
 
 
 @google_login.login_success
@@ -64,21 +45,6 @@ def login_success(token, profile):
 def login_failure(e):
   return jsonify(error=str(e))
 
-@linkedin_login.login_success
-def login_success(token, profile):
-  return jsonify(token=token, profile=profile)
-
-@linkedin_login.login_failure
-def login_failure(e):
-  return jsonify(error=str(e))
-  
-@facebook_login.login_success
-def login_success(token, profile):
-  return jsonify(token=token, profile=profile)
-
-@facebook_login.login_failure
-def login_failure(e):
-  return jsonify(error=str(e))
 
 
 if __name__ == "__main__":
